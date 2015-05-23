61ad7f6d06a093e71cb277f3fb2469bcd35660cf
Toon Sevrin <twan123@live.be>
2015-05-23 21:49:37 +0200

    Added some basic classes

---
+++ src/com/exorath/gameAPI/Game/lobby/Lobby.java
@@ -0,0 +1,7 @@
+package com.exorath.gameAPI.game.lobby;
+
+/**
+ * Created by too on 23/05/2015.
+ */
+public class Lobby {
+}
---
+++ src/com/exorath/gameAPI/lib/Properties.java
@@ -0,0 +1,7 @@
+package com.exorath.gameAPI.lib;
+
+/**
+ * Created by too on 23/05/2015.
+ */
+public class Properties {
+}
