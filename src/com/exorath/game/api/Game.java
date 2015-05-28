d1a43b5150f0b94f66fe999f2a45822fab04ddd8
Toon Sevrin <twan123@live.be>
2015-05-28 19:59:25 +0200

    Little commit, wont do as much as planned today

--- src/com/exorath/game/api/Game.java
+++ src/com/exorath/game/api/Game.java
@@ -1,15 +1,19 @@
 package com.exorath.game.api;
 
+import com.exorath.game.GameAPI;
 import com.exorath.game.api.lobby.Lobby;
 import com.exorath.game.api.teams.TeamManager;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+
 
 /**
  * Created by too on 23/05/2015.
  * Gamemode, RepeatingMinigame and Minigame will extend this.
  */
 
-public abstract class Game {
-    
+public abstract class Game implements Listener {
+
     public static final String DEFAULT_GAME_NAME = "Game";
     public static final String DEFAULT_GAME_DESCRIPTION = "A fun to play game!";
 
@@ -17,19 +21,22 @@ public abstract class Game {
     private TeamManager teamManager = new TeamManager();
     private Lobby lobby = new Lobby();
 
-    private boolean running;
-    
+    private boolean running = false;
+
     public Game() {
+        //register event handler for this game
+        Bukkit.getPluginManager().registerEvents(this, GameAPI.getInstance());
         //this.init();
 
     }
-    
+
     protected void init() {
         this.properties = new Properties();
         this.lobby = new Lobby();
         this.teamManager = new TeamManager();
     }
     public void startGame(){
+        if(running) return;
         running = true;
     }
     public void stopGame() {
@@ -38,27 +45,27 @@ public abstract class Game {
     public Lobby getLobby() {
         return this.lobby;
     }
-    
+
     public TeamManager getTeamManager() {
         return this.teamManager;
     }
-    
+
     public Properties getProperties() {
         return this.properties;
     }
-    
+
     protected void setLobby( Lobby lobby ) {
         this.lobby = lobby;
     }
-    
+
     protected void setTeamManager( TeamManager teamManager ) {
         this.teamManager = teamManager;
     }
-    
+
     protected void setProperties( Properties properties ) {
         this.properties = properties;
     }
-    
+
     public void setName( String name ) {
         this.properties.set( GameProperty.NAME, name );
     }
@@ -72,5 +79,8 @@ public abstract class Game {
 
     public String getDescription( String description ) {
         return this.properties.as( GameProperty.DESCRIPTION, String.class );
+    }
+    public boolean isStarted(){
+        return running;
     }
 }
