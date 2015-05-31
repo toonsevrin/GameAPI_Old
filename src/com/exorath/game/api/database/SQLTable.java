3c8a1d7face5b56a6196d8894bb6e2960697b1cb
Toon Sevrin <twan123@live.be>
2015-05-31 22:21:36 +0200

    Working on SQL

--- src/com/exorath/game/api/database/SQLTable.java
+++ src/com/exorath/game/api/database/SQLTable.java
@@ -2,6 +2,9 @@ package com.exorath.game.api.database;
 
 import com.exorath.game.GameAPI;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by too on 31/05/2015.
  */
@@ -23,6 +26,29 @@ public class SQLTable {
     }
     public void setData(String key, SQLData data){
         //TODO: If exists: update. Else: Insert.
+
+        if(rowExists(key)){
+            createRow(key, data);
+        }else{
+            updateRow(key, data);
+        }
+    }
+    public void createRow(String key, SQLData data){
+        //create new row with data
+    }
+    public void updateRow(String key, SQLData data){
+        //update row with data
+    }
+    public boolean rowExists(String key){
+        try {
+            ResultSet rs = GameAPI.getSQLManager().executeQuery("SELECT EXISTS(SELECT 1 FROM" + name + " WHERE " + KEY + "='" + key + "')");
+            if (rs == null) return false;
+            if (rs.next()) return true;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+
     }
     public void addColumn(String columnName, ColumnType type){
         addColumn(columnName, type.getName());
