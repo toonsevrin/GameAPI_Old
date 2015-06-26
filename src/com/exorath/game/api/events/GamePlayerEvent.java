dfecb558799b749fc14f459ca761f2b5b18532ee
nickrobson <robson.nicholas1@gmail.com>
2015-06-26 10:45:03 +1000

    Add GamePlayerEvent for anything that we may need it for.

---
+++ src/com/exorath/game/api/events/GamePlayerEvent.java
@@ -0,0 +1,22 @@
+package com.exorath.game.api.events;
+
+import com.exorath.game.api.Game;
+import com.exorath.game.api.player.GamePlayer;
+
+/**
+ * @author Nick Robson
+ */
+public abstract class GamePlayerEvent extends GameEvent {
+    
+    private final GamePlayer player;
+    
+    public GamePlayerEvent( Game game, GamePlayer player ) {
+        super( game );
+        this.player = player;
+    }
+    
+    public GamePlayer getPlayer() {
+        return this.player;
+    }
+    
+}
