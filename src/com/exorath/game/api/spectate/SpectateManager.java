b0121b459765e99e0431e5c90dc1869081b12245
nickrobson <robson.nicholas1@gmail.com>
2015-08-24 13:32:00 +1000

    Make SpectateManager actually set players to Spectate mode

--- src/com/exorath/game/api/spectate/SpectateManager.java
+++ src/com/exorath/game/api/spectate/SpectateManager.java
@@ -1,6 +1,10 @@
 package com.exorath.game.api.spectate;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+
 import com.exorath.game.api.Game;
+import com.exorath.game.api.GameProperty;
 import com.exorath.game.api.player.GamePlayer;
 import com.exorath.game.api.player.PlayerState;
 
@@ -8,21 +12,21 @@ import com.exorath.game.api.player.PlayerState;
  * @author Nick Robson
  */
 public class SpectateManager {
-    
+
     private Game game;
-    
+
     public SpectateManager( Game game ) {
         this.game = game;
     }
-    
+
     public Game getGame() {
         return this.game;
     }
-    
+
     public boolean isSpectating( GamePlayer player ) {
         return this.game.getPlayers().getPlayerState( player ) == PlayerState.SPECTATING;
     }
-    
+
     public void setSpectating( GamePlayer player, boolean spectating ) {
         if ( spectating ) {
             this.addSpectator( player );
@@ -30,13 +34,23 @@ public class SpectateManager {
             this.removeSpectator( player );
         }
     }
-    
+
     public void addSpectator( GamePlayer player ) {
         this.game.getPlayers().setState( player, PlayerState.SPECTATING );
+
+        Player p = player.getBukkitPlayer();
+        if ( p != null ) {
+            p.setGameMode( GameMode.SPECTATOR );
+        }
     }
-    
+
     public void removeSpectator( GamePlayer player ) {
         this.game.getPlayers().remove( player );
+
+        Player p = player.getBukkitPlayer();
+        if ( p != null ) {
+            p.setGameMode( game.getProperties().as( GameProperty.DEFAULT_GAMEMODE, GameMode.class ) );
+        }
     }
-    
+
 }
