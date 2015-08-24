4d15b5f451ff4e57cf8ddfca2be38bc8f805d150
nickrobson <robson.nicholas1@gmail.com>
2015-08-24 13:09:45 +1000

    Convert EasyMenu#EasyMenuItem to a FunctionalInterface

--- src/com/exorath/game/api/menu/EasyMenu.java
+++ src/com/exorath/game/api/menu/EasyMenu.java
@@ -15,25 +15,26 @@ import com.google.common.collect.Maps;
  * @author Nick Robson
  */
 public abstract class EasyMenu extends Menu {
-    
+
+    @FunctionalInterface
     public static interface EasyMenuItem {
-        
+
         void onClick( InventoryClickEvent event, Game game, GamePlayer player );
-        
+
     }
-    
+
     private final Map<Integer, EasyMenuItem> metas = Maps.newHashMap();
-    
+
     public EasyMenu( int size ) {
         super( size );
     }
-    
+
     public EasyMenu setItem( int index, ItemStack item, EasyMenuItem meta ) {
         super.setItem( index, item );
         this.metas.put( index, meta );
         return this;
     }
-    
+
     @Override
     public void onClick( InventoryClickEvent event, Game game, GamePlayer player ) {
         for ( int i = 0; i < event.getInventory().getSize(); i++ ) {
@@ -43,11 +44,11 @@ public abstract class EasyMenu extends Menu {
             }
         }
     }
-    
+
     @Override
     public void onOpen( InventoryOpenEvent event, Game game, GamePlayer player ) {}
-    
+
     @Override
     public void onClose( InventoryCloseEvent event, Game game, GamePlayer player ) {}
-    
+
 }
--- src/com/exorath/game/api/npc/types/KitSelector.java
+++ src/com/exorath/game/api/npc/types/KitSelector.java
@@ -3,12 +3,9 @@ package com.exorath.game.api.npc.types;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Zombie;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
 import com.exorath.game.api.Game;
 import com.exorath.game.api.kit.Kit;
 import com.exorath.game.api.menu.EasyMenu;
-import com.exorath.game.api.menu.EasyMenu.EasyMenuItem;
 import com.exorath.game.api.npc.NPCEquipment;
 import com.exorath.game.api.npc.SpawnedNPC;
 import com.exorath.game.api.player.GamePlayer;
@@ -17,24 +14,24 @@ import com.exorath.game.api.player.GamePlayer;
  * @author Nick Robson
  */
 public class KitSelector extends AbstractNPC {
-    
+
     private final Kit kit;
-    
+
     public KitSelector() {
         super( ChatColor.GREEN + "Kit Selector", new NPCEquipment() );
         this.kit = null;
     }
-    
+
     public KitSelector( Kit kit ) {
         super( ChatColor.GREEN + "Select: " + ChatColor.AQUA + kit.getName(), kit.toNPCEquipment() );
         this.kit = kit;
     }
-    
+
     @Override
     public Class<? extends LivingEntity> getEntityClass() {
         return Zombie.class;
     }
-    
+
     @Override
     public void onClicked( Game game, GamePlayer player, SpawnedNPC npc ) {
         if ( this.kit != null ) {
@@ -44,34 +41,19 @@ public class KitSelector extends AbstractNPC {
             player.openMenu( new KitSelectorMenu( game ) );
         }
     }
-    
-    public static class KitMenuItem implements EasyMenuItem {
-        
-        private final Kit kit;
-        
-        public KitMenuItem( Kit kit ) {
-            this.kit = kit;
-        }
-        
-        @Override
-        public void onClick( InventoryClickEvent event, Game game, GamePlayer player ) {
-            this.kit.give( player, game );
-        }
-        
-    }
-    
+
     public static class KitSelectorMenu extends EasyMenu {
-        
+
         public KitSelectorMenu( Game game ) {
             super( 36 );
             int slot = 0;
             for ( Kit kit : game.getKitManager().getKits() ) {
                 if ( slot < 36 ) {// TODO: Pages
-                    this.setItem( slot++, kit.getIcon(), new KitMenuItem( kit ) );
+                    this.setItem( slot++, kit.getIcon(), ( e, g, p ) -> kit.give( p, g ) );
                 }
             }
         }
-        
+
     }
-    
+
 }
