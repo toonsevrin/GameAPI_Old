cf00ec699e3b4bcb9aae31fc47ffb47714547205
Toon Sevrin <twan123@live.be>
2015-09-06 18:57:59 +0200

    Quick fix

---
+++ src/com/exorath/game/api/gametype/minigame/StopReason.java
@@ -0,0 +1,13 @@
+package com.exorath.game.api.gametype.minigame;
+
+/**
+ * Created by TOON on 9/6/2015.
+ */
+public enum StopReason {
+    TIME_LIMIT_REACHED,
+    NOT_ENOUGH_PLAYERS,
+    DRAW,
+    VICTORY,
+    FORCE_STOP
+
+}
